0a17017be53547cf5ab7b4512ef33fc7bec1b45e
Rob <rmuchall@protonmail.com>
2021-09-26 12:24:00 +0100

    Initial code commit

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+        commonjs: true
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: "./tsconfig.json"
+    },
+    plugins: [
+        "@typescript-eslint"
+    ],
+    ignorePatterns: [
+        ".eslintrc.js",
+        "jest.config.js"
+    ],
+    rules: {
+        // Style - Enable
+        "quotes": ["error", "double"],
+        "semi": ["error", "always"],
+        // Nodejs - Enable
+        "require-await": "error",
+        "no-return-await": "error",
+        "@typescript-eslint/explicit-module-boundary-types": "error",
+        "@typescript-eslint/unbound-method": ["error", {"ignoreStatic": true}],
+        // NodeJs - Disable
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-inferrable-types": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/restrict-template-expressions": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+    }
+};
---
+++ src/CacheBustPlugin.ts
@@ -0,0 +1,38 @@
+import webpack from "webpack";
+import Compiler = webpack.Compiler;
+import Compilation = webpack.Compilation;
+import RawSource = webpack.sources.RawSource;
+import {readFileSync} from "fs";
+import path from "path";
+
+export class CacheBustPlugin {
+    apply(compiler: Compiler): void {
+        // const Log = compiler.getInfrastructureLogger("cache-bust-plugin");
+
+        // Hook: thisCompilation
+        compiler.hooks.thisCompilation.tap("cache-bust-plugin", (compilation: Compilation) => {
+            // Hook: processAssets
+            compilation.hooks.processAssets.tap(
+                {
+                    name: "cache-bust-plugin",
+                    stage: Compilation.PROCESS_ASSETS_STAGE_REPORT,
+                },
+                (assets) => {
+                    let htmlTemplate = readFileSync(path.resolve(process.cwd(), "./index.html"), "utf8");
+
+                    // Only cache bust in production
+                    if (process.env["NODE_ENV"] === "production") {
+                        for (const chunk of Array.from(compilation.chunks)) {
+                            for (const file of Array.from(chunk.files)) {
+                                // TODO: Should really use contentHash here
+                                htmlTemplate = htmlTemplate.replace(file, `${file}?hash=${chunk.hash}`);
+                            }
+                        }
+                    }
+
+                    compilation.emitAsset("index.html", new RawSource(htmlTemplate));
+                }
+            );
+        });
+    }
+}
